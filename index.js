73d3e61470e8251dcfcc0b0222d9b2bdedecfd50
Sodynoizz_TH <86451119+Sodynoizz@users.noreply.github.com>
2022-04-11 18:54:22 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+function botdiscord() {window.open('https://discordapp.com/oauth2/authorize?client_id=953977530715623445&scope=bot+applications.commands&permissions=0','_blank');}
+function serverdiscord()  {window.open('https://dsc.gg/taiyaki_world','_blank');}
+function github() {window.open('https://github.com/Sodynoizz','_blank');}
+function youtube()  {window.open('https://www.youtube.com/channel/UCddf6Z2ZR6xtrUwXmLG809Q','_blank');}
