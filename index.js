60ffe0fb477473eb8cfeafbee90814dd41a87049
Sodynoizz_TH <86451119+Sodynoizz@users.noreply.github.com>
2022-04-19 10:19:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function botdiscord() {window.open('https://discordapp.com/oauth2/authorize?client_id=953977530715623445&scope=bot+applications.commands&permissions=0','_blank');}
-function serverdiscord()  {window.open('https://dsc.gg/taiyaki_world','_blank');}
-function github() {window.open('https://github.com/Sodynoizz','_blank');}
-function youtube()  {window.open('https://www.youtube.com/channel/UCddf6Z2ZR6xtrUwXmLG809Q','_blank');}
+function youtube()  {window.open('https://www.youtube.com/channel/UCddf6Z2ZR6xtrUwXmLG809Q','_blank')}
+function github()   {window.open('https://github.com/Sodynoizz','_blank')}
+function serverdiscord()    {window.open('https://dsc.gg/taiyaki_world','_blank')}
+function botdiscord()   {window.open('https://discord.com/api/oauth2/authorize?client_id=963702649012777001&permissions=8&scope=bot%20applications.commands','_blank')}
