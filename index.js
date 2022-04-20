c4d7172d8fb68e7d29b88aa3df69e615ee6d7857
Sodynoizz_TH <86451119+Sodynoizz@users.noreply.github.com>
2022-04-20 21:39:16 +0700

    Update index.js
    Change new support server
    From Taiyaki World server to Raiden Support server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function youtube()  {window.open('https://www.youtube.com/channel/UCddf6Z2ZR6xtrUwXmLG809Q','_blank')}
 function github()   {window.open('https://github.com/Sodynoizz','_blank')}
-function serverdiscord()    {window.open('https://dsc.gg/taiyaki_world','_blank')}
+function serverdiscord()    {window.open('https://dsc.gg/raiden-support','_blank')}
 function botdiscord()   {window.open('https://discord.com/api/oauth2/authorize?client_id=963702649012777001&permissions=8&scope=bot%20applications.commands','_blank')}
